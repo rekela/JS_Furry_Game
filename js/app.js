b21d149c856a942afb138d7fc8064b57b15f443e
rekela <ela.kawczak@gmail.com>
2020-08-05 18:28:06 +0200

    app.js - furry, coin and game constructors added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,40 @@
-console.log('hello');
-const score = document.querySelector('#score');
-console.log(score);
+class Furry{
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.direction = "right";
+    }
+}
+
+class Coin{
+    constructor(){
+        this.x = Math.floor(Math.random() * 10);
+        this.y = Math.floor(Math.random() * 10);
+    }
+}
+
+class Game{
+    constructor(){
+        this.boardElements = document.querySelector('#board').querySelectorAll('div');
+        this.furry = new Furry();
+        this.coin = new Coin();
+        this.score = 0;
+    }
+
+    index(x,y){
+        return x + (y * 10);
+    }
+
+    showFurry(){
+        this.boardElements[this.index(this.furry.x, this.furry.y)].classList.add('furry');
+    }
+
+    showCoin(){
+        this.boardElements[this.index(this.coin.x, this.coin.y)].classList.add('coin');
+    }
+}
+
+const game = new Game;
+game.showFurry();
+game.showCoin();
+
